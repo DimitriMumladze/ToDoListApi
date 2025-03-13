504012e4b353b5607c233f822017e33becf3ec3d
Dimitri <kenwo.dm@gmail.com>
2025-03-13 12:20:11 +0400

    added fluent validation

---
+++ ToDoListApi.Application/ToDoTasks/Commands/ToDoTaskSingleCreate/ToDoTaskSingleCreateCommandValidator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+
+namespace ToDoListApi.Application.ToDoTasks.Commands.ToDoTaskSingleCreate;
+
+public class ToDoTaskSingleCreateCommandValidator : AbstractValidator<ToDoTasksSingleCreateCommand>
+{
+    public ToDoTaskSingleCreateCommandValidator()
+    {
+        RuleFor(dto => dto.Title)
+            .NotEmpty()
+            .WithMessage("Please insert title");
+
+        RuleFor(dto => dto.Description)
+            .NotEmpty()
+            .WithMessage("Please insert description");
+
+        RuleFor(dto => dto.PriorityId)
+            .NotEmpty()
+            .WithMessage("Please insert Id for priority");
+
+        RuleFor(dto => dto.StatusId)
+            .NotEmpty()
+            .WithMessage("Please insert Id for status");
+
+        RuleFor(dto => dto.DueToDate)
+            .NotEmpty()
+            .WithMessage("Please insert due date");
+    }
+}
